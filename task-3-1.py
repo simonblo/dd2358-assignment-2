470a217c39abe674a238a6ba047553e282efded9
Simon Blom <102825244+simonblo@users.noreply.github.com>
2023-02-08 16:12:03 +0100

    Completed task 3.1

---
+++ task-3-1.py
@@ -0,0 +1,37 @@
+import pytest
+
+x1, x2, y1, y2 = -1.8, 1.8, -1.8, 1.8
+c_real, c_imag = -0.62772, -0.42193
+
+def test_julia_set():
+    assert sum(julia_set(1000, 300)) == 33219980
+
+def julia_set(desired_width, max_iterations):
+    x_step = (x2 - x1) / desired_width
+    y_step = (y1 - y2) / desired_width
+    x = []
+    y = []
+    ycoord = y2
+    while ycoord > y1:
+        y.append(ycoord)
+        ycoord += y_step
+    xcoord = x1
+    while xcoord < x2:
+        x.append(xcoord)
+        xcoord += x_step
+    zs = []
+    cs = []
+    for ycoord in y:
+        for xcoord in x:
+            zs.append(complex(xcoord, ycoord))
+            cs.append(complex(c_real, c_imag))
+    output = [0] * len(zs)
+    for i in range(len(zs)):
+        n = 0
+        z = zs[i]
+        c = cs[i]
+        while abs(z) < 2 and n < max_iterations:
+            z = z * z + c
+            n += 1
+        output[i] = n
+    return output
